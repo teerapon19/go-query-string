b0960ad4a6371c488bcd94b3657186948f458b72
Teerapon Nampakdee <teerapon@kkumail.com>
2023-11-22 17:00:51 +0700

    [fix] change exported to unexported type

--- utils.go
+++ utils.go
@@ -1,10 +1,8 @@
 package query
 
 import (
-	"reflect"
 	"regexp"
 	"strings"
-	"testing"
 )
 
 const (
@@ -17,14 +15,8 @@ const (
 var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
 
-func ConvertToSnakeCase(name string) string {
+func convertToSnakeCase(name string) string {
 	snake := matchFirstCap.ReplaceAllString(name, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
-}
-
-func Equal(t *testing.T, expect interface{}, actual interface{}) {
-	if !reflect.DeepEqual(expect, actual) {
-		t.Fatalf("expect: %v, actual: %v", expect, actual)
-	}
 }
